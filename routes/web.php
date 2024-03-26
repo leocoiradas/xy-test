04406fab8c6283a98cfc4c999559cef788a61779
leocoiradas <coiradasalan@gmail.com>
2024-03-26 11:42:09 -0300

    Creada versión de rutas para tareas y empleados
    Se agruparon las rutas por versión (en este caso 'v1') y se indicaron las rutas en las cuales se ejecutará cada controlador.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers'], function() {
+    Route::apiResource('employees', EmployeeController::class);
+    Route::apiResource('tasks', TaskController::class);
+});
