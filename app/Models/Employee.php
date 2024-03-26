fb9b6852c443370ca7c1253f6f37eb52a7d06db5
leocoiradas <coiradasalan@gmail.com>
2024-03-26 09:32:53 -0300

    Creados modelos para empleados y tareas
    Se cambió de nombre el de user por empleados y por eso aparece eliminado

---
+++ app/Models/Employee.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+    protected $fillable = [];
+    /**
+     * Get all of the Task for the User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+}
