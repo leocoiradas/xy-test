f35b91f14c3c70cd13a6d3547ffa2f1fe5e31d7b
leocoiradas <coiradasalan@gmail.com>
2024-03-26 22:46:55 -0300

    Generados los métodos de los controladores que se usarán en el proyecto

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreEmployeeRequest;
 use App\Http\Requests\UpdateEmployeeRequest;
 use App\Http\Resources\EmployeesCollection;
+use App\Http\Resources\EmployeeResource;
+use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
@@ -16,6 +18,10 @@ class EmployeeController extends Controller
     public function index()
     {
         $employees = Employee::all();
+        /*$includeTasks = $request->query('includeTasks');
+        if ($includeTasks){
+            $employees = $employees->with('tasks');
+        }*/
         return new EmployeesCollection($employees);
     }
 
@@ -24,7 +30,7 @@ class EmployeeController extends Controller
      */
     public function create()
     {
-        //
+        return view('employee.create');
     }
 
     /**
@@ -32,31 +38,60 @@ class EmployeeController extends Controller
      */
     public function store(StoreUserRequest $request)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required',
+            'password' => 'required',
+            'email' => 'required',
+            'role' => 'required',
+        ]);
+        $newEmployee = new Employee;
+
+        $newEmployee->name = $request->name;
+        $newEmployee->password = bcrypt($request->password);
+        $newEmployee->email = $request->email;
+        $newEmployee->role = $request->role;
+        $newEmployee->save();
+        return redirect()->route('employee.index');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(User $user)
-    {
-        //
+    public function show($id){
+
+        //$employeeData = $employee->with('tasks')->get();
+
+        $employeeData = Employee::Where('id', '=', $id)->with(['tasks' => function ($query) use ($id) {
+            $query->where('employee_id', $id);
+        }])->get();
+        //dd($employeeData);
+        return new EmployeeResource($employeeData);
+        
     }
+
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(User $user)
+    public function edit(Emoloyee $employee)
     {
-        //
+        //$editEmployee = Employee::findOrFail($employee);
+        return view('employee.edit', compact('employee'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateUserRequest $request, User $user)
+    public function update(UpdateEmployeeRequest $request, Employee $employee)
     {
-        //
+        /*$updateEmployee = Employee::findOrFail($employee);
+        $updateEmployee->name = $request->input('name');
+        $updateEmployee->email = $request->input('email');
+        $updateEmployee->role = $request->input('role');
+        $updateEmployee->save();
+        return redirect()->route('employee.index');*/
+        $employee->update($request->only('name', 'email', 'role'));
+        return redirect()->route('employee.index');
     }
 
     /**
